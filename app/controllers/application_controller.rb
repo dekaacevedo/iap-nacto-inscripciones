77aa4fa450fe893f3006a15fa256bfcdc766d412
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-09-11 12:36:02 -0300

    add devise, navbar, flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :rut, :phone, :admin])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :rut, :phone, :admin])
+  end
 end
---
+++ db/migrate/20210911151907_add_first_name_last_name_rut_phone_admin_to_users.rb
@@ -0,0 +1,9 @@
+class AddFirstNameLastNameRutPhoneAdminToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :rut, :string
+    add_column :users, :phone, :string
+    add_column :users, :admin, :boolean
+  end
+end
